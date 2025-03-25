a4a9c97fed701c8aabd25ec64db55cfc2c339086
Syed Umar Ali <53374123+syed113@users.noreply.github.com>
2025-03-24 22:55:12 -0400

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: true
+  },
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'terser'
+  }
+})
